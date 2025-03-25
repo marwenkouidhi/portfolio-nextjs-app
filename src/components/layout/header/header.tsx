b5cc9ec3f44fc797ceec41dc3bbebf0183a46453
marwenkouidhi <marwenkouidhi@gmail.com>
2025-03-25 16:19:36 +0100

    feat(header): improve contact link scrolling behavior

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -6,8 +6,11 @@ import Link from "next/link";
 import MobileNav from "./header-mobile-nav";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useScrollToAnchor } from "@/hooks/use-scroll-to-anchor";
 
 const Header = () => {
+  const scrollToAnchor = useScrollToAnchor();
+
   return (
     <header className="xl:py-12 px-8 xl:px-0 max-w-screen-lg mx-auto flex items-center justify-between h-32">
       <Link href="/">
@@ -17,8 +20,15 @@ const Header = () => {
       <div className="hidden xl:flex items-center gap-5">
         <HeaderNav />
 
-        <Button asChild>
-          <Link href="#contact">Hire me</Link>
+        <Button
+          className="cursor-pointer"
+          onClick={() =>
+            scrollToAnchor("#contact", {
+              containerId: "landing-container",
+            })
+          }
+        >
+          Hire me
         </Button>
         <ModeToggle />
       </div>
